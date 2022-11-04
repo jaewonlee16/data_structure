c924a09cdc4ee4b6c6369ad33ba05dafa7bdd9c1
jaewonlee16 <19snut@gmail.com>
2022-11-04 18:38:01 +0900

    queue second_commit

--- queue/queue.hpp
+++ queue/queue.hpp
@@ -37,6 +37,41 @@ class Queue{
 template <typename T>
 void Queue<T>::enqueue(const T& value, int priority){
     //TODO
+    // newItem
+    Item<T> newItem;
+    newItem.priority = priority;
+    newItem.value = value;
+    
+    
+    if (empty){
+    	array[front] = newItem;
+    	rear = front;
+    	empty = false;
+    	
+    	return;
+	}
+	
+	if (isFull()){
+		// double array size
+		
+		//copy
+		Item<T> *newArray = new Item<T>[size * 2];
+		for (int i = 0; i < size; i++){
+			newArray[i] = array[(i + front) % size];
+		}
+		delete [] array;
+		//paste
+		array = newArray;
+		front = 0;
+		rear = size - 1;
+		size *= 2;
+		empty = false;
+		
+	}
+	
+	// enqueue
+	array[++rear] = newItem;
+	
     return;
 }
 
@@ -44,17 +79,49 @@ template <typename T>
 int Queue<T>::top(){
     //TODO
     //returning the array index of the highest priority item
-    return NULL;
+    int index = front;
+    int max_priority = -1;
+    int max_priority_index = front;
+    
+    while (index != rear){
+    	if (array[index].priority > max_priority){
+    		max_priority = array[index].priority;
+    		max_priority_index = index;
+		}
+		
+		index = (index + 1) % size;
+	}
+	
+	// when index == rear
+	if (array[index].priority > max_priority){
+    	max_priority = array[index].priority;
+    	max_priority_index = index;
+	}
+		
+	
+    return max_priority_index;
 }
 
 template <typename T>
 T Queue<T>::dequeue(){
     //TODO
+    if (front == rear){
+    	// do something
+    	
+    	empty = true;
+	}
     return NULL;
 }
 
 template <typename T>
 bool Queue<T>::isFull(){
     //TODO
-    return true;
+    if (front < rear){
+    	if (!empty && front == 0 && rear == size - 1)
+    		return true;
+	}
+	else if (rear + 1 == front)
+		return true;
+	else	
+    	return false;
 }
